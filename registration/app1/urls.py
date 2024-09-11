7da9a20ef8e4b1919030d2c825223c6ff787c745
nikhilrajpk <nikhilrajpk2002@gmail.com>
2024-09-11 12:34:53 +0530

    create_user added

--- registration/app1/urls.py
+++ registration/app1/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-    path()
+    path('',views.signupPage,name='signup'),
+    path('login/',views.LoginPage,name='login'),
+    path('home/',views.homePage,name='home'),
 ]
--- registration/app1/views.py
+++ registration/app1/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
-
+from django.shortcuts import render,HttpResponse
+from django.contrib.auth.models import User
 # Create your views here.
+def homePage(request):
+    return render(request,'home.html')
+
+def signupPage(request):
+    if request.method == 'POST':
+        uname = request.POST.get('username')
+        email = request.POST.get('email')
+        pass1 = request.POST.get('password1')
+        pass2 = request.POST.get('password2')
+        my_user = User.objects.create_user(uname,email,pass1)
+        my_user.save()
+        return HttpResponse("user has been created successfully !!!")
+        print(uname,email,pass1,pass2)
+        
+    return render(request,'signup.html')
+
+def LoginPage(request):
+    return render(request,'login.html')
