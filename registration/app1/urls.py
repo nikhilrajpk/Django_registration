7d939a60e7068bbec7fa096a4414f1480612953b
nikhilrajpk <nikhilrajpk2002@gmail.com>
2024-09-13 13:42:03 +0530

    Create function done in CRUD operation

--- registration/app1/urls.py
+++ registration/app1/urls.py
@@ -5,8 +5,6 @@ urlpatterns = [
     path('login/',views.LoginPage,name='login'),
     path('home/',views.homePage,name='home'),
     path('logout/',views.logoutPage,name='logout'),
-    path('user/',views.user_list,name='user_list'),
-    path('user/new/',views.user_create,name='user_create'),
-    path('user/update/',views.user_update,name='user_update'),
-    path('user/delete/',views.user_delete,name='user_delete'),
+    path('adminPage/',views.adminPage,name='adminPage'),
+    path('create/',views.create,name='create'),
 ]
--- registration/app1/views.py
+++ registration/app1/views.py
@@ -3,6 +3,7 @@ from django.contrib.auth.models import User
 from django.contrib.auth import authenticate,login,logout
 from django.contrib.auth.decorators import login_required
 from django.views.decorators.cache import never_cache
+from django.contrib import messages
 # Create your views here.
 
 # @login_required(login_url='login')
@@ -39,12 +40,16 @@ def LoginPage(request):
     if request.user.is_authenticated:
         return redirect(homePage)
     if request.method == 'POST':
-        username = request.POST.get('username')
+        username = request.POST.get('username')        
         pass1 = request.POST.get('pass')
         user = authenticate(request,username = username,password = pass1)
+        
         if user is not None:
             login(request,user)
-            return redirect(homePage)
+            if user.is_superuser:
+                return redirect(adminPage)
+            else:
+                return redirect(homePage)
         else:
             result = "username or password is incorrect!"
             return render(request,'login.html',{'result':result})
@@ -57,15 +62,25 @@ def logoutPage(request):
         logout(request)
     return redirect(LoginPage)
 
+@never_cache
+def adminPage(request):
+    if request.user.is_authenticated:
+        user_obj = User.objects.all().filter(is_staff = False)
+        context = {'user_obj' : user_obj}
+        # print(user_obj.values())
+        return render(request,'adminPage.html',context)
+    else:
+        return redirect(signupPage)
+@never_cache
+def create(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        email = request.POST.get('email') 
+        password = request.POST.get('password')
+        
+        user_obj = User.objects.create_user(username, email, password)
+        print(user_obj)
+        user_obj.save()
+        
+        return redirect(adminPage)  
 
-def user_list(request):
-    return render(request,'user_list.html')
-
-def user_create(request):
-    return render(request,'user_form.html')
-
-def user_update(request):
-    return render(request,'user_form.html')
-
-def user_delete(request):
-    return render(request,'user_confirm_delete')
